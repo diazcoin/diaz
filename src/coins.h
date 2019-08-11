07f9cc5634074cf8d9e85033825ec17c6b7ac0fd
diazcoin <diazcoin1@gmail.com>
2019-08-11 09:35:22 +0200

    rename from bitcoin to diaz

--- src/coins.h
+++ src/coins.h
@@ -282,7 +282,7 @@ public:
     size_t DynamicMemoryUsage() const;
 
     /**
-     * Amount of bitcoins coming in to a transaction
+     * Amount of diazs coming in to a transaction
      * Note that lightweight clients may not know anything besides the hash of previous transactions,
      * so may not be able to calculate this.
      *
@@ -319,7 +319,7 @@ const Coin& AccessByTxid(const CCoinsViewCache& cache, const uint256& txid);
 /**
  * This is a minimally invasive approach to shutdown on LevelDB read errors from the
  * chainstate, while keeping user interface out of the common library, which is shared
- * between diazd, and bitcoin-qt and non-server tools.
+ * between diazd, and diaz-qt and non-server tools.
  *
  * Writes do not need similar protection, as failure to write is handled by the caller.
 */
--- src/util/system.cpp
+++ src/util/system.cpp
@@ -666,7 +666,7 @@ static std::string FormatException(const std::exception* pex, const char* pszThr
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(nullptr, pszModule, sizeof(pszModule));
 #else
-    const char* pszModule = "bitcoin";
+    const char* pszModule = "diaz";
 #endif
     if (pex)
         return strprintf(
@@ -685,13 +685,13 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 
 fs::path GetDefaultDataDir()
 {
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Bitcoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Bitcoin
-    // Mac: ~/Library/Application Support/Bitcoin
-    // Unix: ~/.bitcoin
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Diaz
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Diaz
+    // Mac: ~/Library/Application Support/Diaz
+    // Unix: ~/.diaz
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Diaz";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -701,10 +701,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/Diaz";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".diaz";
 #endif
 #endif
 }
@@ -1190,9 +1190,9 @@ std::string CopyrightHolders(const std::string& strPrefix)
     const auto copyright_devs = strprintf(_(COPYRIGHT_HOLDERS).translated, COPYRIGHT_HOLDERS_SUBSTITUTION);
     std::string strCopyrightHolders = strPrefix + copyright_devs;
 
-    // Make sure Bitcoin Core copyright is not removed by accident
-    if (copyright_devs.find("Bitcoin Core") == std::string::npos) {
-        strCopyrightHolders += "\n" + strPrefix + "The Bitcoin Core developers";
+    // Make sure Diaz Core copyright is not removed by accident
+    if (copyright_devs.find("Diaz Core") == std::string::npos) {
+        strCopyrightHolders += "\n" + strPrefix + "The Diaz Core developers";
     }
     return strCopyrightHolders;
 }
