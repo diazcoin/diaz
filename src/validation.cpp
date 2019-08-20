2b6dce7f8da90e1ba5382a4edb4838ba447bb9bd
diazcoin <diazcoin1@gmail.com>
2019-08-20 19:50:02 +0100

    changed reward value

--- src/validation.cpp
+++ src/validation.cpp
@@ -1031,12 +1031,21 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
     int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
     // Force block reward to zero when right shift is undefined.
-    if(nHeight == 1)
-        return 800000 * COIN;
+    CAmount nSubsidyBase;
+
+   if (nHeight == 1 ) {
+        nSubsidyBase =  800000;
+    } else if (nHeight == 4) {
+        nSubsidyBase = 0.85;
+    } else if (nHeight == 5) {
+	nSubsidyBase = 0.9;
+    } else {
+	nSubsidyBase = 1;
+    }
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 0.0005 * COIN;
+    CAmount nSubsidy = nSubsidyBase * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
